4bd3a48f54291e4a69715e1b5e2468d758d7b95f
Scott Jann <sjann@knight-rider.org>
2013-06-06 16:20:32 -0500

    Initial Commit

---
+++ FixMPEG/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  FixMPEG
+//
+//  Created by Scott Jann on 6/6/13.
+//  Copyright (c) 2013 trms.com. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ FixMPEG/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FixMPEG
+//
+//  Created by Scott Jann on 6/6/13.
+//  Copyright (c) 2013 trms.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
